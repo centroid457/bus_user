445fc6443e52b3f87caaa3ea70d79dadf4b341f2
Andrei Starichenko <a.starichenko@element-t.ru>
2023-12-08 12:51:44 +0300

    add info in print docs

--- serial_port/main.py
+++ serial_port/main.py
@@ -13,29 +13,60 @@ class SerialPort:
     @classmethod
     def print(cls) -> None:
         """
-        ==========OBJECTINFO.PRINT==========================================================================
-        str=COM8 - PL2303HXA PHASED OUT SINCE 2012. PLEASE CONTACT YOUR SUPPLIER.
-        repr=<serial.tools.list_ports_common.ListPortInfo object at 0x00000267B3A38200>
-        ----------properties_ok-----------------------------------------------------------------------------
-        description              	str       :COM8
-        device                   	str       :COM8
-        hwid                     	str       :USB VID:PID=067B:2303 SER= LOCATION=1-2.4.3
-        interface                	NoneType  :None
-        location                 	str       :1-2.4.3
-        manufacturer             	str       :Prolific
-        name                     	str       :COM8
-        pid                      	int       :8963
-        product                  	NoneType  :None
-        serial_number            	str       :
-        vid                      	int       :1659
-        ----------properties_exx----------------------------------------------------------------------------
-        ----------objects-----------------------------------------------------------------------------------
-        ----------methods_ok--------------------------------------------------------------------------------
-        apply_usb_info           	NoneType  :None
-        usb_description          	str       :COM8
-        usb_info                 	str       :USB VID:PID=067B:2303 SER= LOCATION=1-2.4.3
-        ----------methods_exx-------------------------------------------------------------------------------
-        ====================================================================================================
+        WINDOWS - USB
+            ==========OBJECTINFO.PRINT==========================================================================
+            str=COM8 - PL2303HXA PHASED OUT SINCE 2012. PLEASE CONTACT YOUR SUPPLIER.
+            repr=<serial.tools.list_ports_common.ListPortInfo object at 0x00000267B3A38200>
+            ----------properties_ok-----------------------------------------------------------------------------
+            description              	str       :COM8
+            device                   	str       :COM8
+            hwid                     	str       :USB VID:PID=067B:2303 SER= LOCATION=1-2.4.3
+            interface                	NoneType  :None
+            location                 	str       :1-2.4.3
+            manufacturer             	str       :Prolific
+            name                     	str       :COM8
+            pid                      	int       :8963
+            product                  	NoneType  :None
+            serial_number            	str       :
+            vid                      	int       :1659
+            ----------properties_exx----------------------------------------------------------------------------
+            ----------objects-----------------------------------------------------------------------------------
+            ----------methods_ok--------------------------------------------------------------------------------
+            apply_usb_info           	NoneType  :None
+            usb_description          	str       :COM8
+            usb_info                 	str       :USB VID:PID=067B:2303 SER= LOCATION=1-2.4.3
+            ----------methods_exx-------------------------------------------------------------------------------
+            ====================================================================================================
+
+        RASPBERRY - USB
+            ==========OBJECTINFO.PRINT==========================================================================
+            str=/dev/ttyUSB0 - USB-Serial Controller
+            repr=<serial.tools.list_ports_linux.SysFS object at 0x7fb332d9d0>
+            ----------properties_ok-----------------------------------------------------------------------------
+            description                     str       :USB-Serial Controller
+            device                          str       :/dev/ttyUSB0
+            device_path                     str       :/sys/devices/platform/soc/3f980000.usb/usb1/1-1/1-1.4/1-1.4:1.0/ttyUSB0
+            hwid                            str       :USB VID:PID=067B:2303 LOCATION=1-1.4
+            interface                       NoneType  :None
+            location                        str       :1-1.4
+            manufacturer                    str       :Prolific Technology Inc.
+            name                            str       :ttyUSB0
+            pid                             int       :8963
+            product                         str       :USB-Serial Controller
+            serial_number                   NoneType  :None
+            subsystem                       str       :usb-serial
+            usb_device_path                 str       :/sys/devices/platform/soc/3f980000.usb/usb1/1-1/1-1.4
+            usb_interface_path              str       :/sys/devices/platform/soc/3f980000.usb/usb1/1-1/1-1.4/1-1.4:1.0
+            vid                             int       :1659
+            ----------properties_exx----------------------------------------------------------------------------
+            ----------objects-----------------------------------------------------------------------------------
+            ----------methods_ok--------------------------------------------------------------------------------
+            apply_usb_info                  NoneType  :None
+            usb_description                 str       :USB-Serial Controller
+            usb_info                        str       :USB VID:PID=067B:2303 LOCATION=1-1.4
+            ----------methods_exx-------------------------------------------------------------------------------
+            read_line                       TypeError :join() missing 1 required positional argument: 'a'
+            ====================================================================================================
         """
         port_obj_list = list_ports.comports()
 
