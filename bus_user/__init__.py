63e20e2c098512cebe49189ea087e7dfb698d405
Andrei Starichenko <a.starichenko@element-t.ru>
2023-12-14 15:50:27 +0300

    solve import only Lunux supported module

--- bus_user/__init__.py
+++ bus_user/__init__.py
@@ -1,2 +1,8 @@
-from .bus_serial import *
 from .history import *
+from .bus_serial import *
+
+# from .bus_i2c import *  # import only Linux supported modul return exx
+try:
+    from .bus_i2c import *    # import only Linux supported modul return exx
+except:
+    pass
