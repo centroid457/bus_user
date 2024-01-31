d9d277a09b76633f810dc0641aa945c79fe7f824
Andrei Starichenko <a.starichenko@element-t.ru>
2024-01-31 12:28:13 +0300

    apply new build

--- _share-3=build.bat
+++ _share-3=build.bat
@@ -1,5 +1,23 @@
+rem ====================================
+rem VERSION = (0, 0, 1)  # use new build
+
+rem ====================================
+echo off
+cls
+
+rem DEL OLD DIST =======================
 REM del dist\ /q /s
 rd dist\ /q /s
+rd build\ /q /s
+cls
 
-python setup.py sdist bdist_wheel
+rem BUILD ==============================
+rem old ---------------
+rem python setup.py sdist bdist_wheel
+
+rem new ---------------
+rem python -m build     # this is from docs but NOT WORKING (/build/* creation has ERROR)
+python -m build --sdist --wheel
+
+rem FINISH =============================
 pause
