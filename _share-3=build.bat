bf1a1d9e93fa6ee10cf41220f69226674c5c02dd
Andrei Starichenko <a.starichenko@element-t.ru>
2024-01-31 13:03:12 +0300

    apply new build VERSION = (0, 0, 2)

--- _share-3=build.bat
+++ _share-3=build.bat
@@ -1,5 +1,6 @@
 rem ====================================
 rem VERSION = (0, 0, 1)  # use new build
+rem VERSION = (0, 0, 2)  # separate build/dist cmds
 
 rem ====================================
 echo off
@@ -17,7 +18,19 @@ rem python setup.py sdist bdist_wheel
 
 rem new ---------------
 rem python -m build     # this is from docs but NOT WORKING (/build/* creation has ERROR)
-python -m build --sdist --wheel
+rem python -m build --sdist --wheel     # it will definitely start parts by pipe!
+
+echo ------------------------------------
+echo ------------------------------------
+echo ------------------------------------
+echo --------- create [DIST/*] ----------
+python -m build --sdist
+
+echo ------------------------------------
+echo ------------------------------------
+echo ------------------------------------
+echo --------- create [BUILD/*] ---------
+python -m build --wheel
 
 rem FINISH =============================
 pause
