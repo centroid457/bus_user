e86b5b880d885a4cb9bd181dd14e9b39a1ea0ff4
Andrei Starichenko <centroid@mail.ru>
2024-05-24 15:38:00 +0300

    zero corr Test__Shorted_validateModel_InfinitRW

--- test__serial_client__validate_model.py
+++ test__serial_client__validate_model.py
@@ -14,45 +14,82 @@ from bus_user import *
 # =====================================================================================================================
 # @pytest.mark.skip
 class Test__Shorted_validateModel_InfinitRW:
-    Victim: Type[SerialClient]
+    """
+    VALIDATE ADAPTERS fro DECODУ ERRORS
+    """
     victim: SerialClient
 
-    @classmethod
-    def setup_class(cls):
-        class Victim(SerialClient):
-            ADDRESS = Type__AddressAutoAcceptVariant.FIRST_FREE__SHORTED
-
-        cls.Victim = Victim
-        cls.victim = cls.Victim()
-        if not cls.victim.connect():
-            msg = f"[ERROR] not found PORT shorted by Rx+Tx"
-            print(msg)
-            raise Exception(msg)
-
-    @classmethod
-    def teardown_class(cls):
-        if cls.victim:
-            cls.victim.disconnect()
-
-    # def setup_method(self, method):
+    # @classmethod
+    # def setup_class(cls):
     #     pass
     #
-    # def teardown_method(self, method):
+    # @classmethod
+    # def teardown_class(cls):
+    #     if cls.victim:
+    #         cls.victim.disconnect()
+    #
+    # def setup_method(self, method):
     #     pass
 
+    def teardown_method(self, method):
+        pass
+        if self.victim:
+            self.victim.disconnect()
+
     # -----------------------------------------------------------------------------------------------------------------
-    def test__1(self):
+    def test__shorted(self):
         """
         connect shorted port and start this code!
         wait for first error and see the step number
         write as result in SerialClient docstring
         """
+        # PREPARE ------------------------
+        class Victim(SerialClient):
+            ADDRESS = Type__AddressAutoAcceptVariant.FIRST_FREE__SHORTED
+
+        self.victim = Victim()
+        if not self.victim.connect():
+            msg = f"[ERROR] not found PORT shorted by Rx+Tx"
+            print(msg)
+            raise Exception(msg)
+
+        # START WORKING ------------------
         index = 0
         while True:
             index += 1
             load = f"step {index}"
             print(load)
             assert self.victim.write_read_line_last(load) == load
+
+    def test__real_device(self):    # TODO: fix it!!!
+        """
+        in case of validate real device like ATC/PTB
+        DONT FORGET TO CHANGE address__answer_validation for your device!!!
+        """
+        # PREPARE ------------------------
+        VALIDATION_CMD, VALIDATION_ANSWER = ("GET NAME", "ATC 03")
+
+        class Victim(SerialClient):
+            ADDRESS = Type__AddressAutoAcceptVariant.FIRST_FREE__ANSWER_VALID
+
+            def address__answer_validation(self) -> bool:
+                answer = self.write_read_line_last(VALIDATION_CMD)
+                print(f"{answer=}")
+                return answer == VALIDATION_ANSWER
+
+        self.victim = Victim()
+        if not self.victim.connect():
+            msg = f"[ERROR] not found PORT validated"
+            print(msg)
+            raise Exception(msg)
+
+        # START WORKING ------------------
+        index = 0
+        while True:
+            index += 1
+            load = f"step {index}"
+            print(load)
+            assert self.victim.connect__validation() is True
 
 
 # =====================================================================================================================
