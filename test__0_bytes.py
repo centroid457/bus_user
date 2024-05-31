4bdb4ddac0d5e5b7f12f5fc4880eb85a0348899a
Andrei Starichenko <centroid@mail.ru>
2024-05-31 11:19:02 +0300

    start tests by new pytest_aux style

---
+++ test__0_bytes.py
@@ -0,0 +1,36 @@
+import pytest
+from typing import *
+
+import time
+import pathlib
+import shutil
+from tempfile import TemporaryDirectory
+
+from bus_user import *
+from pytest_aux import *
+
+
+# =====================================================================================================================
+@pytest.mark.parametrize(argnames="func_link", argvalues=[SerialClient._data_ensure__bytes, ])
+@pytest.mark.parametrize(
+    argnames="args, _EXPECTED",
+    argvalues=[
+        (111, Exception),
+        (str, Exception),
+
+        ("", b""),
+        ("str", b"str"),
+        ("str\n", b"str\n"),
+        ("str\r\n", b"str\r\n"),
+
+        (b"", b""),
+        (b"str", b"str"),
+        (b"str\n", b"str\n"),
+        (b"str\r\n", b"str\r\n"),
+    ]
+)
+def test__data_ensure__bytes(func_link, args, _EXPECTED):
+    pytest_func_tester__wo_kwargs(func_link, args, _EXPECTED)
+
+
+# =====================================================================================================================
