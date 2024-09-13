ec5d055a514e7a917c2c9f7646b88cff54e6a2c3
Andrei Starichenko <centroid@mail.ru>
2024-09-13 11:59:22 +0300

    [Serial] explore stable connection

--- explore.py
+++ explore.py
@@ -2,17 +2,33 @@
 # useful to start smth without pytest and not to run in main script!
 
 from bus_user import *
+from typing import *
+import time
 
 
 class Dev(SerialClient_FirstFree):
+    BAUDRATE: int = 115200
+    EOL__SEND = b"\r\n"
+    RAISE_CONNECT = False
     LOG_ENABLE = True
-    EOL__SEND: bytes = b"\n"
-    BAUDRATE = 115200
+    # TIMEOUT__READ: float = 3       # 0.2 is too short!!! dont touch! in case of reading char by char 0.5 is the best!!! 0.3 is not enough!!!
+
+    # def address__validate(self) -> bool | None | NoReturn:
+    #     for i in range(5):
+    #         self._write('help')
+    #         if self.read_line():
+    #             break
+    #         time.sleep(1)
+    #     self.buffers_clear()
 
 
 dev = Dev()
-assert dev.connect()
-print(f"{dev.ADDRESS=}")
-
-print(dev.write_read__last("*:GET NAME"))
-# print(dev._write(b"\n"))
+if dev.connect():
+    for i in range(10):
+        dev.write_read__last('stop')
+        time.sleep(1)
+        print()
+        print()
+        print()
+        print()
+        print()
